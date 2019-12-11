bb9c4dc93dcaba1055183cb7520f016d3bb185e0
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-12-11 05:22:44 -0600

    [FIX] fixed user modal issues

--- public/js/player.js
+++ public/js/player.js
@@ -41,28 +41,45 @@ const getPlayerAnswer = (event) => {
     showMessageModal(correctAnswer)
 }
 
+const updateCurrentQuestion = () => {
+    firebase.database().ref('currentQuestion').set({...currentQuestion, isAnswered:true});
+}
+
 const showMessageModal = ( status ) => {
     switch ( status ) {
         case true:
             $('#message-modal .modal-title').text("¡¡¡Felicidades!!!");
             $('#message-modal .modal-text').text(`Tu respuesta es correcta, has ganado ${questionScore} puntos.`);
-            $('#message-modal').modal('show');
+            $('#message-modal').addClass("answered").modal('show');
+            $('#message-modal').on('shown.bs.modal', () => {
+                setTimeout(() => {
+                    $('#message-modal').removeClass('answered').modal('hide')
+                },2000)
+            })
+            updateCurrentQuestion();
             break;
         
         case false:
-            $('#message-modal .modal-title').text("¡¡¡Nooooo!!!");
-            $('#message-modal .modal-text').text("Tu respuesta es incorrecta, ¡suerte para la próxima!");
+            $('#message-modal:not(".answered") .modal-title').text("¡¡¡Nooooo!!!");
+            $('#message-modal:not(".answered") .modal-text').text("Tu respuesta es incorrecta, ¡suerte para la próxima!");
             $('#message-modal').modal('show');
+            $('#message-modal').on('shown.bs.modal', () => {
+                setTimeout(() => {
+                    $('#message-modal').removeClass('answered').modal('hide')
+                },2000)
+            })
             break;
 
         case 'answered':
-            $('#message-modal .modal-title').text("¡¡¡Ups!!!");
-            $('#message-modal .modal-text').text("Alguien ha respondido antes que tú, ¡suerte para la próxima!");
+            $('#message-modal:not(".answered") .modal-title').text("¡¡¡Ups!!!");
+            $('#message-modal:not(".answered") .modal-text').text("Alguien ha respondido antes que tú, ¡suerte para la próxima!");
             $('#message-modal').modal('show');
+            $('#message-modal').on('shown.bs.modal', () => {
+                setTimeout(() => {
+                    $('#message-modal').removeClass('answered').modal('hide')
+                },2000)
+            })
             break;
     }
 }
 
-const updateCurrentQuestion = () => {
-    firebase.database().ref('currentQuestion').set({...currentQuestion, isAnswered:true});
-}
