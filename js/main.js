89b0aacea000f17e657eeaa6119a1e7a1b2ec100
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-12-10 19:10:02 -0600

    [FEAT] added player view and current question interaction

--- js/main.js
+++ js/main.js
@@ -16,74 +16,66 @@ let questions = {
     }, {
         "answers": [{
             "isCorrect": true,
-            "text": "id"
+            "text": "RealTime Database"
         }, {
             "isCorrect": false,
-            "text": "href"
+            "text": "Firebase Database"
         }, {
             "isCorrect": false,
-            "text": "name"
+            "text": "Cloud Storage"
         }],
         "category": "HTML",
         "points": 200,
-        "text": "¿Cuál de los siguientes atributos HTML sirve para asignar un identificador único a un elemento?"
+        "text": "¿Cuál es el nombre del servicio que nos permite generar una base de datos en tiempo real?"
     }, {
         "answers": [{
+            "isCorrect": false,
+            "text": "px"
+        }, {
+            "isCorrect": false,
+            "text": "rem"
+        }, {
             "isCorrect": true,
-            "text": "id"
-        }, {
-            "isCorrect": false,
-            "text": "href"
-        }, {
-            "isCorrect": false,
-            "text": "name"
+            "text": "vem"
         }],
         "category": "CSS",
-        "points": 200,
-        "text": "¿Cuál de los siguientes atributos HTML sirve para asignar un identificador único a un elemento?"
+        "points": 300,
+        "text": "¿Cuál de las siguientes no es una unidad de medida válida en CSS?"
     }, {
         "answers": [{
             "isCorrect": false,
-            "text": "<p></p>"
+            "text": "En cualquier lugar está bien"
+        }, {
+            "isCorrect": false,
+            "text": "Junto al botón que invocará dicha Modal"
         }, {
             "isCorrect": true,
-            "text": "<span></span>"
-        }, {
-            "isCorrect": false,
-            "text": "<div></div>"
-        }],
-        "category": "JS",
-        "points": 500,
-        "text": "¿Cuál de los siguientes tagname corresponde a un elemento de línea?"
-    }, {
-        "answers": [{
-            "isCorrect": true,
-            "text": "id"
-        }, {
-            "isCorrect": false,
-            "text": "href"
-        }, {
-            "isCorrect": false,
-            "text": "name"
+            "text": "Al inicio del documento"
         }],
         "category": "BOOTSTRAP",
-        "points": 300,
-        "text": "¿Cuál de los siguientes atributos HTML sirve para asignar un identificador único a un elemento?"
+        "points": 500,
+        "text": "¿En qué lugar de mi documento de HTML se debe colocar el snippet del componente 'Modal'?"
     }, {
         "answers": [{
+            "isCorrect": false,
+            "text": ".toggle()"
+        }, {
             "isCorrect": true,
-            "text": "id"
+            "text": ".toggleClass()"
         }, {
             "isCorrect": false,
-            "text": "href"
-        }, {
-            "isCorrect": false,
-            "text": "name"
+            "text": ".exchangeClass()"
         }],
         "category": "JS / JQUERY",
-        "points": 100,
-        "text": "¿Cuál de los siguientes atributos HTML sirve para asignar un identificador único a un elemento?"
+        "points": 400,
+        "text": "Este método de JQuery sirve para intercambiar entre dos clases de un elemento:"
     }]
+}
+
+var database = firebase.database();
+
+const setCurrentQuestion = question => {
+    firebase.database().ref('currentQuestion/0').set(question);
 }
 
 let activeCells;
@@ -122,13 +114,15 @@ const setSelectedQuestion = () => {
     console.log(questions.questions[questionIndex])
     let selectedQuestion = questions.questions[questionIndex];
     let answers = selectedQuestion.answers;
+    $('.question-category').text(selectedQuestion.category)
     $('.question-text').text(selectedQuestion.text);
     $('.answers-wrapper').empty();
     answers.forEach((answer, index) => {
         $('.answers-wrapper').append(`
-            <div class='btn btn-block btn-outline-secondary'>${answer.text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')}</div>
+            <div data-answer-id=${index} class='btn btn-block btn-outline-secondary'>${answer.text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')}</div>
         `)
     })
+    setCurrentQuestion(selectedQuestion)
 }
 
 fillGrid();
